e3fafa9b77c1710f34b4f549291934aafb34b8f7
REAGAN2020 <reaganteflon@gmail.com>
2020-10-14 02:42:26 +0300

    added teardown for social test file

--- social_test.py
+++ social_test.py
@@ -23,11 +23,11 @@ class TestCredentials(unittest.TestCase):
         self.new_credentials.save_credentials()  
         self.assertEqual(len(Credentials.credentials_list), 1)
 
-    # def tearDown(self):
-    #         '''
-    #         tearDown method that does clean up after each test case has run.
-    #         '''
-    #         Credentials.credentials_list = []
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Credentials.credentials_list = []
             
     # def test_save_multiple_credentials(self):
     #     '''
