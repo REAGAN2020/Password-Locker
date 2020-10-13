90422e895915b8fb1772b6e2e73c26aa3536af75
REAGAN2020 <reaganteflon@gmail.com>
2020-10-14 00:09:20 +0300

    added decorators

--- user.py
+++ user.py
@@ -15,3 +15,22 @@ class User:
         '''
 
         User.user_list.append(self)
+
+    def delete_user(self):
+        '''
+        method that deletes user from user list
+        '''
+        User.user_list.remove(self)
+
+    @classmethod
+    def find_by_name(cls, name):
+        for user in cls.user_list:
+            if user in cls.user_list == name:
+                return user
+
+
+    @classmethod
+    def user_exist(cls, name):
+        '''
+        method that checks if user exists
+        '''
